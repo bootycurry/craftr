dd5a610dd9d8dcef90b3958e65a74a2a3e1b0ef7
Harshit Vootukuri <hvootukuri@gmail.com>
2025-08-16 04:36:44 +0530

    feat(logger): updated the log display to use LogType enum to display corresponding log messages with respective styles

--- src/craftrEditor/log_display.cpp
+++ src/craftrEditor/log_display.cpp
@@ -1,79 +1,75 @@
 #include <craftrEditor/log_display.hpp>
-#include <iostream>
-#include <gtkmm/enums.h>
 #include <gtkmm/cssprovider.h>
-
+#include <gtkmm/enums.h>
+#include <iostream>
 
 LogDisplay::LogDisplay()
-: reset_button("Reset Log"),
-main_box(Gtk::Orientation::VERTICAL)
-{
+    : reset_button("Reset Log"), main_box(Gtk::Orientation::VERTICAL) {
   reset_button.signal_clicked().connect([this]() {
-        auto buffer = log_text.get_buffer();
-        buffer->set_text("");
-    });
+    auto buffer = log_text.get_buffer();
+    buffer->set_text("");
+  });
 
-  scroll_container.set_policy(Gtk::PolicyType::AUTOMATIC, Gtk::PolicyType::AUTOMATIC);
+  scroll_container.set_policy(Gtk::PolicyType::AUTOMATIC,
+                              Gtk::PolicyType::AUTOMATIC);
   scroll_container.set_child(log_text);
   main_box.append(reset_button);
   main_box.append(scroll_container);
   scroll_container.set_hexpand(true);
   scroll_container.set_vexpand(true);
-  
 
   auto css_provider = Gtk::CssProvider::create();
   css_provider->load_from_data(
-      "textview text { background-color: #000000; font-size: 22px}" 
-  );
+      "textview text { background-color: #000000; font-size: 22px; }");
 
   auto context = log_text.get_style_context();
-  context->add_provider(
-      css_provider,
-      GTK_STYLE_PROVIDER_PRIORITY_USER
-  );
+  context->add_provider(css_provider, GTK_STYLE_PROVIDER_PRIORITY_USER);
 
-  
   Glib::RefPtr<Gtk::TextBuffer> buffer = log_text.get_buffer();
   Glib::RefPtr<Gtk::TextTagTable> tag_table = buffer->get_tag_table();
   auto tag = Gtk::TextTag::create("INFO");
-  tag -> property_foreground() = "#0000FF";
-  tag_table -> add(tag);
+  tag->property_foreground() = "#0000FF";
+  tag_table->add(tag);
   tag = Gtk::TextTag::create("ERROR");
-  tag -> property_foreground() = "#FF0000";
-  tag_table -> add(tag);
+  tag->property_foreground() = "#FF0000";
+  tag_table->add(tag);
   tag = Gtk::TextTag::create("DEBUG");
-  tag -> property_foreground() = "#00FF00";
-  tag_table -> add(tag);
+  tag->property_foreground() = "#00FF00";
+  tag_table->add(tag);
   tag = Gtk::TextTag::create("WARNING");
-  tag -> property_foreground() = "#FFFF00";
-  tag_table -> add(tag);
-  
-
-
+  tag->property_foreground() = "#FFFF00";
+  tag_table->add(tag);
 }
 
+LogDisplay::~LogDisplay() {}
 
-LogDisplay::~LogDisplay()
-{
-}
+void LogDisplay::log(const Glib::ustring &message, const LogType log_type) {
+  Glib::ustring tag_name;
+  Glib::ustring prefix;
 
-void LogDisplay::log(const Glib::ustring& message, bool replace)
-{
-  
-  
-  Glib::RefPtr<Gtk::TextBuffer> buffer = log_text.get_buffer();
-  Glib::RefPtr<Gtk::TextTagTable> tag_table = buffer->get_tag_table();
-  Glib::RefPtr<Gtk::TextTag> tag;
-  if (replace)
-  {
-    tag = tag_table -> lookup("INFO");
+  switch (log_type) {
+  case DEBUG:
+    tag_name = "DEBUG";
+    prefix = "[DEBUG] ";
+    break;
+  case WARNING:
+    tag_name = "WARNING";
+    prefix = "[WARNING] ";
+    break;
+  case ERROR:
+    tag_name = "ERROR";
+    prefix = "[ERROR] ";
+    break;
+  case INFO:
+  default:
+    tag_name = "INFO";
+    prefix = "[INFO] ";
+    break;
   }
 
-  
-  buffer -> insert_with_tag(buffer -> end(), "\n" + message + "\n", tag);
-  
-  
-  
-  
-}
+  Glib::RefPtr<Gtk::TextBuffer> buffer = log_text.get_buffer();
+  Glib::RefPtr<Gtk::TextTag> tag = buffer->get_tag_table()->lookup(tag_name);
+  Glib::ustring final_message = "\n" + prefix + message + "\n";
 
+  buffer->insert_with_tag(buffer->end(), final_message, tag);
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,79 @@
-#include <iostream>
 #include <glad/glad.h>
+#include <iostream>
 #include <logger.hpp>
 
-#include <gtkmm/window.h>
 #include <gtkmm/application.h>
-#include <gtkmm/paned.h>
 #include <gtkmm/notebook.h>
+#include <gtkmm/paned.h>
+#include <gtkmm/window.h>
 
-
-#include <craftrEditor/log_window.hpp>
 #include <craftrEditor/log_display.hpp>
+#include <craftrEditor/log_window.hpp>
 
-Gtk::Window* temp_main_window = nullptr;
-Gtk::Window* log_window = nullptr;
+Gtk::Window *temp_main_window = nullptr;
+Gtk::Window *log_window = nullptr;
 
+class MainWindow : public Gtk::Window {
+public:
+  MainWindow() {
+    hor = Gtk::Paned(Gtk::Orientation::VERTICAL);
+    top = Gtk::Paned(Gtk::Orientation::HORIZONTAL);
+    bot = Gtk::Paned(Gtk::Orientation::HORIZONTAL);
 
-class MainWindow : public Gtk::Window
-{
-  public:
-    MainWindow(){
-      hor = Gtk::Paned(Gtk::Orientation::VERTICAL);
-      top = Gtk::Paned(Gtk::Orientation::HORIZONTAL);
-      bot = Gtk::Paned(Gtk::Orientation::HORIZONTAL);
+    hor.set_start_child(top);
+    hor.set_end_child(bot);
+    top.set_start_child(top_left);
+    top.set_end_child(top_right);
+    bot.set_start_child(bottom_left);
+    bot.set_end_child(bottom_right);
 
-      hor.set_start_child(top);
-      hor.set_end_child(bot);
-      top.set_start_child(top_left);
-      top.set_end_child(top_right);
-      bot.set_start_child(bottom_left);
-      bot.set_end_child(bottom_right);
+    bottom_right.append_page(log_display.main_box);
 
-      
-      bottom_right.append_page(log_display.main_box);
+    set_child(hor);
+    log_display.log("application initialized", INFO);
+    log_display.log("application initialized", DEBUG);
+    log_display.log("application initialized", WARNING);
+    log_display.log("application initialized", ERROR);
+  };
+  ~MainWindow() override {};
 
-      set_child(hor);
+protected:
+  Gtk::Notebook top_left;
+  Gtk::Notebook top_right;
+  Gtk::Notebook bottom_left;
+  Gtk::Notebook bottom_right;
 
-    };
-    ~MainWindow() override{};
-  
-  protected:
-    Gtk::Notebook top_left;
-    Gtk::Notebook top_right;
-    Gtk::Notebook bottom_left;
-    Gtk::Notebook bottom_right;
+  LogDisplay log_display;
 
-    LogDisplay log_display;
-
-  private:
-    Gtk::Paned hor;
-    Gtk::Paned top;
-    Gtk::Paned bot;
+private:
+  Gtk::Paned hor;
+  Gtk::Paned top;
+  Gtk::Paned bot;
 };
 
-class CraftrEditorAppTemp : public Gtk::Application
-{
-  protected:
-    void on_activate() override {
-      // Create your windows and keep them alive as member variables
-      main_win = std::make_unique<MainWindow>();
-      // log_win  = std::make_unique<LogWindow>();
-      main_win->set_title("Main Window");
-      main_win->set_default_size(800, 600);
-      
-      add_window(*main_win);
-      // add_window(*log_win);
+class CraftrEditorAppTemp : public Gtk::Application {
+protected:
+  void on_activate() override {
+    // Create your windows and keep them alive as member variables
+    main_win = std::make_unique<MainWindow>();
+    // log_win  = std::make_unique<LogWindow>();
+    main_win->set_title("Main Window");
+    main_win->set_default_size(800, 600);
 
-      main_win->show();
-      
-      // log_win->show();
+    add_window(*main_win);
+    // add_window(*log_win);
+
+    main_win->show();
+
+    // log_win->show();
   }
 
-  private:
-    std::unique_ptr<MainWindow> main_win;
-    // std::unique_ptr<LogWindow> log_win;
+private:
+  std::unique_ptr<MainWindow> main_win;
+  // std::unique_ptr<LogWindow> log_win;
 };
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char *argv[]) {
   auto app = std::make_shared<CraftrEditorAppTemp>();
   return app->run(argc, argv);
-  
-  
-  
 }
