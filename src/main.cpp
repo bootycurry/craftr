de9a2adea5a9cee351d0d20396969313858df3d4
Harshit Vootukuri <hvootukuri@gmail.com>
2025-08-16 14:07:25 +0530

    chore(logger): removed the old logger ui files

--- src/main.cpp
+++ src/main.cpp
@@ -8,10 +8,8 @@
 #include <gtkmm/window.h>
 
 #include <craftrEditor/log_display.hpp>
-#include <craftrEditor/log_window.hpp>
 
 Gtk::Window *temp_main_window = nullptr;
-Gtk::Window *log_window = nullptr;
 
 class MainWindow : public Gtk::Window {
 public:
@@ -56,21 +54,16 @@ protected:
   void on_activate() override {
     // Create your windows and keep them alive as member variables
     main_win = std::make_unique<MainWindow>();
-    // log_win  = std::make_unique<LogWindow>();
     main_win->set_title("Main Window");
     main_win->set_default_size(800, 600);
 
     add_window(*main_win);
-    // add_window(*log_win);
 
     main_win->show();
-
-    // log_win->show();
   }
 
 private:
   std::unique_ptr<MainWindow> main_win;
-  // std::unique_ptr<LogWindow> log_win;
 };
 
 int main(int argc, char *argv[]) {
