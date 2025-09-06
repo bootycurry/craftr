cb2fa7b5e4f0cbf208b412bcc21bf5b058c352fa
Harshit Vootukuri <hvootukuri@gmail.com>
2025-09-06 18:08:31 +0530

    feat(vector2): implemented default constructor for Vector2

--- include/craftr_engine/vector2.hpp
+++ include/craftr_engine/vector2.hpp
@@ -1,20 +1,19 @@
 #ifndef CRAFTR_VECTOR2_HPP
 #define CRAFTR_VECTOR2_HPP
 
-class Vector2{
-    public:
-        float x;
-        float y;
+class Vector2 {
+public:
+  float x;
+  float y;
 
-        Vector2 operator*(const int& other) const;
-        Vector2 operator+(const Vector2& other) const;
-        Vector2 operator-(const Vector2& other) const;
-        
+  Vector2 operator*(const int &other) const;
+  Vector2 operator+(const Vector2 &other) const;
+  Vector2 operator-(const Vector2 &other) const;
 
-        Vector2(int x, int y);
-
+  Vector2(int x, int y);
+  Vector2();
 };
 
-Vector2 operator*(const int& scalar, const Vector2& vec);
+Vector2 operator*(const int &scalar, const Vector2 &vec);
 
 #endif
